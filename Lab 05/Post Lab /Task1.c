f33eb8c39c756c5646a298825e9243c783b3b81d
SyedAli-Shah <shahali8642@gmail.com>
2026-09-23 19:12:13 +0500

    Done the Task 1

--- Lab 05/Post Lab /Task1.c
+++ Lab 05/Post Lab /Task1.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main(){
+    float temp;
+    printf("Enter the temperature in Celsius:");
+    scanf("%f",&temp);
+    if (temp<15)
+    {printf("Cold");}
+    else if (temp>15&&temp<30)
+    {printf("Normal");}
+    else {printf("Hot");}
+    return 0;
+}
